bee6bcffb081a99d228488e41a6f66664fa84576
Gui Santana <104835205+GuiSantana0FC@users.noreply.github.com>
2023-07-01 14:13:14 -0300

    Add files via upload

---
+++ calcu.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print("Porfavor insira um numero")
+n1 = int(input("--> "))
+print("insira outro numero")
+n2 = int(input("--> "))
+print("Vai ser soma ou subtração? *INSIRA TUDO MINUSCULO E SEM ACENTO!*")
+e1 = input("--> ")
+if e1 == ("soma"):
+    print(n1 + n2)
+
+if e1 == ("subtraçao"):
+    print(n1 - n2)
+    
