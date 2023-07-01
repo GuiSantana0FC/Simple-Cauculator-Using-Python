5571f3bc9308cbbf57f350011d5b6a6be6d8d33c
Gui Santana <104835205+GuiSantana0FC@users.noreply.github.com>
2023-07-01 15:01:01 -0300

    calc.py

--- calcu.py
+++ calcu.py
@@ -1,13 +1,13 @@
-print("Hello, World!")
+print("Hello, World!") #The command *print* serves to show the *Hello, World!* in the terminal.
 print("Please, choose a number.")
-n1 = int(input("--> "))
+n1 = int(input("--> ")) #the command *input* serves to change the variable *n1* using the question *-->* you can change the question every time if you want!
 print("Choose another number.")
-n2 = int(input("--> "))
+n2 = int(input("--> "))  #the *int* variable serves to add value to the *n2* variable, there are several variables such as *int(), float(), bool() and str()* the *int* being to add the value of numbers
 print("Going to be addition or subtraction?")
 e1 = input("--> ")
-if e1 == ("addition"):
-    print(n1 + n2)
+if e1 == ("addition"): #if you put *addition* in *e1* it will show the *print* command in the terminal
+    print(n1 + n2) #the *print* command will take the value of the two variables *n1* *n2* and will add the value of the variables, which will show the result in the terminal
 
-if e1 == ("subtraction"):
+if e1 == ("subtraction"): #if you put something other than *subtraction* it will not show anything in the terminal, because the *else* command was not applied.
     print(n1 - n2)
     
